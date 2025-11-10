72161bc1d509869fd450dd473c436836c03cbb0e
Matteo Mary <20098764@wit.ie>
2025-11-10 22:27:40 +0000

    setup for auth api

---
+++ bin/auth-app.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CognitoStack } from '../lib/cognito-stack';
+ 
+const app = new cdk.App();
+new CognitoStack(app, 'CognitoStack', {
+  env: { region: 'eu-west-1' },
+});
+
